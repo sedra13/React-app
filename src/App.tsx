de0ce93cf97820f689e7ae5b742477ba00e6f58f
Sedra Shakra <sedra.shakra@gmail.com>
2026-05-17 08:18:34 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+//import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
+function App() {
+  const [alertVisible,setAlertVisibility]=useState(false)
+  return (
+    <div>
+     {alertVisible&& <Alert onClose={()=>setAlertVisibility(false)}>Alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true )}>
+        Button
+      </Button>
+    </div>
+  );
+}
+
+/*
+
+function App() {
+
+    let items = ["san francisco","paris", "tokyo", "florida", "london"];
+
+    const handleSelectItem=(item:string)=>{
+
+      console.log(item);
+
+    }
+
+  return (
+
+    <div>
+
+      <ListGroup items={items} heading="cities" onSelectItem={handleSelectItem} />
+
+    </div>
+
+  );
+
+}
+
+ */
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;

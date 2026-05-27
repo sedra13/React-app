87f39123b56c7006d0ffd09b87b44c6586f6acb3
Sedra Shakra <sedra.shakra@gmail.com>
2026-05-27 17:59:59 +0300

    Refactor:Replace alert button with listgroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-//import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import { useState } from "react";
-function App() {
+import ListGroup from "./components/ListGroup";
+//import Alert from "./components/Alert";
+//import Button from "./components/Button";
+//import { useState } from "react";
+/*function App() {
   const [alertVisible,setAlertVisibility]=useState(false)
   return (
     <div>
@@ -14,29 +14,26 @@ function App() {
   );
 }
 
-/*
-
+ 
+*/
 function App() {
+  let items = ["san francisco", "paris", "tokyo", "florida", "london"];
 
-    let items = ["san francisco","paris", "tokyo", "florida", "london"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
-    const handleSelectItem=(item:string)=>{
-
-      console.log(item);
-
-    }
-
-  return (
-
-    <div>
-
-      <ListGroup items={items} heading="cities" onSelectItem={handleSelectItem} />
-
-    </div>
-
-  );
-
+  return (
+    <div>
+      {""}
+      <ListGroup
+        items={items}
+        heading="cities"
+        onSelectItem={handleSelectItem}
+      />
+      {""}
+    </div>
+  );
 }
 
- */
 export default App;
--- src/Message.tsx
+++ src/Message.tsx
@@ -3,3 +3,4 @@ function Message (){
 return<h1>hello world!</h1>
 }
 export default Message;
+ 
